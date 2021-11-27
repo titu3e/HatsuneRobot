f16c8eea214e05f8f0c40b286dd464840fe4859a
ZenitsuXD #M•R•T™ <85629049+ZenitsuXD@users.noreply.github.com>
2021-11-27 21:20:09 +0700

    Hatsune Miku

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m HatsuneRobot
+web: python3 -m HatsuneRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Hatsune Miku
+:: Enables virtual env mode and then starts KURUMI
+env\scripts\activate.bat && py -m HatsuneRobot
