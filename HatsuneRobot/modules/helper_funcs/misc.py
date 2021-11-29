390859595295c44cae3229dd284dc58646358211
ZenitsuXD #M•R•T™ <85629049+ZenitsuXD@users.noreply.github.com>
2021-11-30 04:53:22 +0700

    Hatsune Miku

--- HatsuneRobot/modules/helper_funcs/misc.py
+++ HatsuneRobot/modules/helper_funcs/misc.py
@@ -68,19 +68,14 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
 
-#    pairs = list(zip(modules[::2], modules[1::2]))
-
-#    if len(modules) % 2 == 1:
-#        pairs.append((modules[-1],))
-
-#    max_num_pages = ceil(len(pairs) / 7)
-#   modulo_page = page_n % max_num_pages
+    max_num_pages = ceil(len(pairs) / 7)
+    modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-#    if len(pairs) > 7:
-#        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-#            (EqInlineKeyboardButton("<", callback_data="{}_prev({})".format(prefix, modulo_page)),
-#             EqInlineKeyboardButton(">", callback_data="{}_next({})".format(prefix, modulo_page)))]
+    if len(pairs) > 7:
+        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
+            (EqInlineKeyboardButton("🔺", callback_data="{}_prev({})".format(prefix, modulo_page)),
+             EqInlineKeyboardButton("🔻", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     return pairs
 
