4597e5b3dafa824cdb81fc407009634454a3160a
ZenitsuID #M•R•T™ <85629049+ZenitsuID@users.noreply.github.com>
2021-12-21 15:18:02 +0700

    Hatsune Miku ✨

--- HatsuneRobot/modules/alive.py
+++ HatsuneRobot/modules/alive.py
@@ -1,3 +1,19 @@
+"""Hatsune Robot main module"""
+# Copyright (C) 2021 - 2022  ZenitsuID, <https://github.com/ZenitsuID.git>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import re
 from platform import python_version as kontol
@@ -9,12 +25,12 @@ from HatsuneRobot.events import register
 from HatsuneRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/56811b69cbcece20bbebf.jpg"
+PHOTO = "https://telegra.ph/file/b2f3b095387faad48680d.jpg"
 
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Hatsune Miku Robot.** \n\n"
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Hatsune Miku.** \n\n"
     TEXT += "⚪ **I'm Working Properly** \n\n"
     TEXT += f"⚪ **My Master : [ZenitsuID](https://t.me/ZenitsuID)** \n\n"
     TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
