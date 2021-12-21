003ca2c446871bd3b7684a07b6305ee3681f84e6
ZenitsuID #M•R•T™ <85629049+ZenitsuID@users.noreply.github.com>
2021-12-21 11:25:29 +0700

    Hatsune Miku ✨

--- HatsuneRobot/modules/helper_funcs/misc.py
+++ HatsuneRobot/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("Go Home", callback_data="Hatsune_back")]]
+        pairs += [[EqInlineKeyboardButton("ɢᴏ ʜᴏᴍᴇ", callback_data="hatsune_back")]]
 
     return pairs
 
