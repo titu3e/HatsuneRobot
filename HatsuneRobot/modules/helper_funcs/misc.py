2ea427453e7901616ff6049a47f4617f6caf14fd
ZenitsuID #M•R•T™ <85629049+ZenitsuID@users.noreply.github.com>
2021-12-21 19:32:30 +0700

    Hatsune Miku ✨

--- HatsuneRobot/modules/helper_funcs/misc.py
+++ HatsuneRobot/modules/helper_funcs/misc.py
@@ -66,12 +66,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 4:
         pairs = pairs[modulo_page * 4:4 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("🔙", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🔝", callback_data="hatsune_back"),
-             EqInlineKeyboardButton("🔜", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("↩️", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("ʜᴏᴍᴇ", callback_data="hatsune_back"),
+             EqInlineKeyboardButton("↪️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("🔝", callback_data="hatsune_back")]]
+        pairs += [[EqInlineKeyboardButton("ʜᴏᴍᴇ", callback_data="hatsune_back")]]
 
     return pairs
 
