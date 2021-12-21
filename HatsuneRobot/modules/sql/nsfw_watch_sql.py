8e52641eff905566fc9d7d95d6e1bb339976fcd0
ZenitsuID #M•R•T™ <85629049+ZenitsuID@users.noreply.github.com>
2021-12-21 14:20:56 +0700

    Hatsune Miku ✨

--- HatsuneRobot/modules/sql/nsfw_watch_sql.py
+++ HatsuneRobot/modules/sql/nsfw_watch_sql.py
@@ -1,15 +1,18 @@
-#    Copyright (C) Midhun KM 2020-2021
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+"""Hatsune Robot main module"""
+# Copyright (C) 2021 - 2022  ZenitsuID, <https://github.com/ZenitsuID.git>
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from sqlalchemy import Column, String
 
